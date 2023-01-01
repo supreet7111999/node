5e0596618a18b5a98e229e657ff872c79a141bab
Supreet Kumar <supreet7111999@gmail.com>
2023-01-01 14:59:37 +0530

    vue_new
    vue js tutorials

---
+++ myvue/first_pro/src/components/child_components/child1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="ch1">
+        <h1>
+            Child1
+        </h1>
+    </div>
+</template>
+
+
+<script>
+  export default{
+    name:'child1'
+  }
+</script>
+
+<style>
+  div.ch1{
+    background-color: rgb(145, 126, 66);
+  }
+</style>
---
+++ myvue/first_pro/src/components/cond_rendering/pro1.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <div>
+        <!-- for if and else , we use v-if="condition " and v-else directives-->
+        <!-- directives are used to change or manipulate elements of html -->
+        <h1 v-if="!show">Conditional Rendering if false</h1>
+        <h1 v-else>if true</h1>
+        <button v-on:click="chng">Toggle Conditional Rendering</button>
+    </div>
+</template>
+
+
+<script>
+ export default{
+    name:'cond',
+    props:{
+        
+    },
+    data()//this is data method , which returns the data_properties
+    {
+           return {
+               show:true
+           }
+    },
+    methods:{
+        chng()
+        {
+            if(this.show==true)
+            //    alert(this.show);
+               this.show=false;
+            else
+               this.show=true;   
+        }
+    }
+ };
+</script>
